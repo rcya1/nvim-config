12bf9148f36e5f758127d36827f51956d2990062
Ryan10145 <chang.ryan10145@gmail.com>
2023-01-18 00:52:34 -0500

    Maybe fix bug with not being able to save files (?)

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,11 +4,11 @@ configs.setup {
     "latex",
   },
   sync_install = false,
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
+  -- highlight = {
+  --   enable = true, -- false will disable the whole extension
+  --   disable = { "" }, -- list of language that will be disabled
+  --   additional_vim_regex_highlighting = true,
 
-  },
+  -- },
   indent = { enable = true, disable = { "yaml" } },
 }
